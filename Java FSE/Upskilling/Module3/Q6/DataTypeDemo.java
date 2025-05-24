58207de9c751d24c03ab9932cee70f2ca8012348
SarmisthaMoharer <it2022086@rcciit.org.in>
2025-05-24 15:38:17 +0530

    Java started (1-6 done)

---
+++ Java FSE/Upskilling/Module3/Q6/DataTypeDemo.java
@@ -0,0 +1,15 @@
+class DataTypeDemo {
+    public static void main(String[] args){
+        int num = 21;
+        float num1 = 3.14f;
+        double num2 = 7.89;
+        char myChar = 'S';
+        boolean myBoolean = true;
+
+        System.out.println("int: " + num);
+        System.out.println("float: " + num1);
+        System.out.println("double: " + num2);
+        System.out.println("char: " + myChar);
+        System.out.println("boolean: " + myBoolean);
+    }
+}
